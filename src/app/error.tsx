bc6793e5f49bca31613ad4b003f6e03cf6ac9def
Tooboi <japica123@gmail.com>
2023-12-30 05:22:52 -0500

    removed apos

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export default function ErrorPage() {
-    return <div>Oh dear... You shouldn't be here - Please refresh page</div>
+    return <div>Oh dear... You should not be here - Please refresh page</div>
 }
