7f4c86d45a960f907baed742f3d0c838c2c8706e
Tooboi <japica123@gmail.com>
2024-01-14 14:53:53 -0500

    Styled and Responsive

--- src/app/renders/[id]/page.tsx
+++ src/app/renders/[id]/page.tsx
@@ -39,15 +39,59 @@ export async function generateMetadata({
 
 export default async function RenderPage({ params: { id } }: RenderPageProps) {
   const render = await getRender(id);
+  // const createdAtDate = new Date(render.createdAt).toLocaleDateString("en-US", {
+  //   year: "numeric",
+  //   month: "short",
+  //   day: "numeric",
+  // });
   // console.log(render);
 
   return (
     <div className="container-2xl">
-      <div className="">
-        <div>
+      <div className="lg:hidden mb-4 h-fit flex-row">
+          <div className="p-4 w-full">
+            <h1 className="text-pretty mb-4 text-2xl font-bold">
+              {render.name}
+            </h1>
+            <p className="text-pretty mb-4">{render.description}</p>
+            <div className="mb-4">
+              <p className="text-md font-semibold">Software Used</p>
+              <div className=" flex flex-row gap-1">
+                {render.blender ? (
+                  <Image src={Blender} alt={"Blender"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.maya ? (
+                  <Image src={Maya} alt={"Maya"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.substance ? (
+                  <Image src={Substance} alt={"Substance"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.arnold ? (
+                  <Image src={Arnold} alt={"Arnold"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.zbrush ? (
+                  <Image src={Zbrush} alt={"Zbrush"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+              </div>
+            </div>
+            <p className="text-lg font-bold">{render.year}</p>
+          </div>
+        </div>
+      <div className="lg:grid lg:grid-cols-6 ">
+        <div className="lg:col-span-4 md:col-span-3">
           {/* Map through the ImageCollection array */}
           {render.imageCollection && render.imageCollection.length > 0 && (
-            <div className="flex  flex-col">
+            <div className="mx-auto flex flex-col overflow-hidden rounded-xl">
               {render.imageCollection.map((pubID, index) => (
                 <CldImageWrapped
                   priority
@@ -59,42 +103,49 @@ export default async function RenderPage({ params: { id } }: RenderPageProps) {
                   src={pubID}
                   sizes="100vw"
                   alt={render.name}
-                  className=""
+                  className="w-full"
                 />
               ))}
             </div>
           )}
         </div>
-        <div className="flex-cols flex gap-4 lg:flex-row lg:items-center">
-          <h1 className="text-5xl font-bold">{render.name}</h1>
-          <p className="mt-4">{render.description}</p>
-          <p>{render.year}</p>
-          <div>
-            {render.blender ? (
-              <Image src={Blender} alt={"Blender"} width={32} />
-            ) : (
-              <p></p>
-            )}
-            {render.maya ? (
-              <Image src={Maya} alt={"Maya"} width={32} />
-            ) : (
-              <p></p>
-            )}
-            {render.substance ? (
-              <Image src={Substance} alt={"Substance"} width={32} />
-            ) : (
-              <p></p>
-            )}
-            {render.arnold ? (
-              <Image src={Arnold} alt={"Arnold"} width={32} />
-            ) : (
-              <p></p>
-            )}
-            {render.zbrush ? (
-              <Image src={Zbrush} alt={"Zbrush"} width={32} />
-            ) : (
-              <p></p>
-            )}
+        <div className="lg:col-span-2 md:col-span-3 ml-4 hidden h-fit flex-row rounded-lg border-2 border-stone-700 bg-stone-800 md:flex">
+          <div className="p-4">
+            <h1 className="text-pretty mb-4 text-3xl font-bold">
+              {render.name}
+            </h1>
+            <p className="text-pretty mb-8">{render.description}</p>
+            <div className="">
+              <p className="text-lg font-semibold">Software Used</p>
+              <div className=" flex flex-row gap-1">
+                {render.blender ? (
+                  <Image src={Blender} alt={"Blender"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.maya ? (
+                  <Image src={Maya} alt={"Maya"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.substance ? (
+                  <Image src={Substance} alt={"Substance"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.arnold ? (
+                  <Image src={Arnold} alt={"Arnold"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+                {render.zbrush ? (
+                  <Image src={Zbrush} alt={"Zbrush"} width={32} />
+                ) : (
+                  <p></p>
+                )}
+              </div>
+            </div>
+            <p className="text-right text-lg font-bold">{render.year}</p>
           </div>
         </div>
       </div>
