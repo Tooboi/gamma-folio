efee2993df1d0ac921e03c4c663aae53ad3ead0d
Tooboi <japica123@gmail.com>
2024-01-14 15:58:39 -0500

    Footer, Nav, Responsive

--- src/app/renders/[id]/page.tsx
+++ src/app/renders/[id]/page.tsx
@@ -48,47 +48,47 @@ export default async function RenderPage({ params: { id } }: RenderPageProps) {
 
   return (
     <div className="container-2xl">
-      <div className="lg:hidden mb-4 h-fit flex-row">
-          <div className="p-4 w-full">
-            <h1 className="text-pretty mb-4 text-2xl font-bold">
-              {render.name}
-            </h1>
-            <p className="text-pretty mb-4">{render.description}</p>
-            <div className="mb-4">
-              <p className="text-md font-semibold">Software Used</p>
-              <div className=" flex flex-row gap-1">
-                {render.blender ? (
-                  <Image src={Blender} alt={"Blender"} width={32} />
-                ) : (
-                  <p></p>
-                )}
-                {render.maya ? (
-                  <Image src={Maya} alt={"Maya"} width={32} />
-                ) : (
-                  <p></p>
-                )}
-                {render.substance ? (
-                  <Image src={Substance} alt={"Substance"} width={32} />
-                ) : (
-                  <p></p>
-                )}
-                {render.arnold ? (
-                  <Image src={Arnold} alt={"Arnold"} width={32} />
-                ) : (
-                  <p></p>
-                )}
-                {render.zbrush ? (
-                  <Image src={Zbrush} alt={"Zbrush"} width={32} />
-                ) : (
-                  <p></p>
-                )}
-              </div>
+      <div className="mb-4 h-fit flex-row lg:hidden">
+        <div className="w-full lg:p-4">
+          <h1 className="text-pretty  text-center text-2xl font-bold">
+            {render.name}
+          </h1>
+          <p className="text-lg font-bold text-center">{render.year}</p>
+          <p className="text-pretty mb-4 text-center">{render.description}</p>
+          <div className="mb-4 flex justify-center">
+            {/* <p className="text-md font-semibold">Software Used</p> */}
+            <div className=" flex flex-row gap-1">
+              {render.blender ? (
+                <Image src={Blender} alt={"Blender"} width={28} />
+              ) : (
+                <p></p>
+              )}
+              {render.maya ? (
+                <Image src={Maya} alt={"Maya"} width={28} />
+              ) : (
+                <p></p>
+              )}
+              {render.substance ? (
+                <Image src={Substance} alt={"Substance"} width={28} />
+              ) : (
+                <p></p>
+              )}
+              {render.arnold ? (
+                <Image src={Arnold} alt={"Arnold"} width={28} />
+              ) : (
+                <p></p>
+              )}
+              {render.zbrush ? (
+                <Image src={Zbrush} alt={"Zbrush"} width={28} />
+              ) : (
+                <p></p>
+              )}
             </div>
-            <p className="text-lg font-bold">{render.year}</p>
           </div>
         </div>
+      </div>
       <div className="lg:grid lg:grid-cols-6 ">
-        <div className="lg:col-span-4 md:col-span-3">
+        <div className="md:col-span-3 lg:col-span-4">
           {/* Map through the ImageCollection array */}
           {render.imageCollection && render.imageCollection.length > 0 && (
             <div className="mx-auto flex flex-col overflow-hidden rounded-xl">
@@ -109,7 +109,7 @@ export default async function RenderPage({ params: { id } }: RenderPageProps) {
             </div>
           )}
         </div>
-        <div className="lg:col-span-2 md:col-span-3 ml-4 hidden h-fit flex-row rounded-lg border-2 border-stone-700 bg-stone-800 md:flex">
+        <div className="ml-4 hidden h-fit flex-row rounded-lg border-2 border-stone-700 bg-stone-800 md:col-span-3 md:flex lg:col-span-2">
           <div className="p-4">
             <h1 className="text-pretty mb-4 text-3xl font-bold">
               {render.name}
