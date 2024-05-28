a8d209ea35c255aebe2594f34d335d1417495b98
Tooboi <japica123@gmail.com>
2024-05-28 15:26:23 -0400

    Fixing bugs from package update 2

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/db/prisma";
 import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
