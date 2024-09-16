a07b281e8070033482df2d388cbbb44608201fa7
Tooboi <japica123@gmail.com>
2024-09-16 18:59:37 -0400

    Edit image collection formatted correctly

--- src/components/Wrappers/CldEditImgColWrapper.tsx
+++ src/components/Wrappers/CldEditImgColWrapper.tsx
@@ -34,7 +34,7 @@ function formatBytes(fileSize: number): string {
 export default function CldEditUploadImageWrapper({
   render,
 }: CldEditImgColWrapperProps) {
-  console.log(render.imageCollection);
+  // console.log(render.imageCollection);
 
   // Initialize publicIdsArray with the values from render.imageCollection
   const [publicIdsArray, setPublicIdsArray] = useState<string[]>([]);
@@ -81,57 +81,60 @@ export default function CldEditUploadImageWrapper({
           const publicId = result.info.public_id;
           setButtonClassName("hidden");
           handleSuccess(result);
-          console.log(publicId + "WIDGET");
+          console.log(publicId + "_ADDED");
         }}
       >
         {({ open }) => {
           return (
             <div className="mx-auto flex w-full flex-col justify-center">
-              <button
-                className={buttonClassName}
-                onClick={(e) => {
-                  e.preventDefault();
-                  open();
-                }}
-              >
-                Add Images - Max 10
-              </button>
-              {publicIdsArray.length > 0 && (
-                <>
-                  {publicIdsArray.map((publicId, index) => {
-                    // Check if "gamma-folio" is already present in the thumbnail URL
-                    const thumbnailSrc = publicId.includes("gamma-folio")
-                      ? publicId // If already present, use the URL as it is
-                      : `gamma-folio/${publicId}`; // If not, add "gamma-folio"
+              <div className="pb-4">
+                <button
+                  className={buttonClassName}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    open();
+                  }}
+                >
+                  Add Images - Max 10
+                </button>
+                <div className="flex justify-center">
+                  {publicIdsArray.length > 0 && (
+                    <div className="flex flex-wrap place-content-center">
+                      {publicIdsArray.map((publicId, index) => {
+                        // Check if "gamma-folio" is already present in the thumbnail URL
+                        const thumbnailSrc = publicId.includes("gamma-folio")
+                          ? publicId // If already present, use the URL as it is
+                          : `gamma-folio/${publicId}`; // If not, add "gamma-folio"
 
-                    return (
-                      <div key={index} className="h-full">
-                        <div className="relative overflow-hidden px-2">
-                          <div
-                            onClick={() => removeImage(index)}
-                            className="group absolute right-2 w-8 cursor-pointer rounded-bl-lg rounded-tr-lg border-b-2 border-s-2 border-brand-700 bg-brand-800 transition-all hover:bg-rose-600"
-                          >
-                            <div className="w-8 pr-1 ">
-                              <XMarkIcon className="fill-brand-500 transition-all group-hover:fill-brand-200" />
+                        return (
+                          <div key={index} className="p-2">
+                            <div className="relative overflow-hidden ">
+                              <div
+                                onClick={() => removeImage(index)}
+                                className="group absolute right-0 w-8 cursor-pointer rounded-bl-lg rounded-tr-lg border-b-2 border-s-2 border-brand-700 bg-brand-800 transition-all hover:border-rose-500 hover:bg-rose-700"
+                              >
+                                <div className="w-8 pr-1 ">
+                                  <XMarkIcon className="fill-brand-500 transition-all group-hover:fill-rose-300" />
+                                </div>
+                              </div>
+                              <CldImageWrapper
+                                alt={`Thumbnail ${index}`}
+                                src={`${thumbnailSrc}`}
+                                width="256"
+                                height="256"
+                                crop="fill"
+                                aspectRatio="1:1"
+                                sizes="100vw"
+                                className="mx-auto rounded-lg border-2 border-brand-700"
+                              />
                             </div>
                           </div>
-                          <CldImageWrapper
-                            alt={`Thumbnail ${index}`}
-                            // Pass the thumbnailSrc to generate the Cloudinary image URL
-                            src={`${thumbnailSrc}`}
-                            width="256"
-                            height="256"
-                            crop="fill"
-                            aspectRatio="1:1"
-                            sizes="100vw"
-                            className="mx-auto rounded-lg border-2 border-brand-700"
-                          />
-                        </div>
-                      </div>
-                    );
-                  })}
-                </>
-              )}
+                        );
+                      })}
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           );
         }}
