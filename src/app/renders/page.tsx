d40a91771474fd683078d34f01b78260617cc450
Josh Pica <japica123@gmail.com>
2026-03-27 11:59:48 -0400

    created NSFW page

--- src/app/renders/page.tsx
+++ src/app/renders/page.tsx
@@ -21,16 +21,24 @@ export default async function Renders({
   const pageSize = 24;
   const heroItemCount = 0;
 
-  const totalItemCount = await prisma.render.count();
+const totalItemCount = await prisma.render.count({
+  where: {
+    NSFW: false,
+  },
+});
 
   const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize);
 
-  const renders = await prisma.render.findMany({
-    orderBy: { createdAt: "desc" },
-    skip:
-      (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
-    take: pageSize + (currentPage === 1 ? heroItemCount : 0),
-  });
+const renders = await prisma.render.findMany({
+  where: {
+    NSFW: false,
+  },
+  orderBy: { createdAt: "desc" },
+  skip:
+    (currentPage - 1) * pageSize +
+    (currentPage === 1 ? 0 : heroItemCount),
+  take: pageSize + (currentPage === 1 ? heroItemCount : 0),
+});
   return (
     <div className="flex flex-col items-center">
       <div className="max-w-8xl mx-auto min-h-screen pb-6 pt-4 md:px-4 lg:px-10">
@@ -43,8 +51,6 @@ export default async function Renders({
       {totalPages > 1 && (
         <PaginationBar currentPage={currentPage} totalPages={totalPages} />
       )}
-      {/* <GoogleAnalytics gaId='G-DF8DN8Q9LH' dataLayerName="RenderPage" />
-      <GoogleTagManager gtmId="GTM-KMB769RD" /> */}
     </div>
   );
 }
